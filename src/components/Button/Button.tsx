b9b16c970226ec39ada2f3635bc778fa425c6b94
fwbrandao <fwbrandaobeta@gmail.com>
2023-04-12 08:55:06 -0300

    feat: added jest tests and babel

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import styled from 'styled-components'
+
+
+const StyledButton = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0.5em 1em;
+  padding: 0.25em 1em;
+`
 
 interface ButtonProps {
   label: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return  <button>{props.label}</button>
+  return <StyledButton>{props.label}</StyledButton>
 };
 
 export default Button;
